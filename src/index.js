97a7d982b99132b0a5480171290e5b72a13fd629
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-04-12 14:51:14 -0700

    Requisito 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
-
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+
+app.get('/talker', async (req, res) => {
+  try {
+    const data = await fs.readFile('src/talker.json', 'utf8');
+    const talkers = JSON.parse(data);
+
+    if (talkers.length === 0) {
+      res.status(HTTP_OK_STATUS).json([]);
+    } else {
+      res.status(HTTP_OK_STATUS).json(talkers);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Palestrante não encontrado' });
+  }
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
