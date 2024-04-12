587485a364fdeed52b37710cebd3b3c306284996
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-04-12 15:05:29 -0700

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const PORT = process.env.PORT || '3001';
 
 app.get('/talker', async (req, res) => {
@@ -18,7 +19,26 @@ app.get('/talker', async (req, res) => {
       res.status(HTTP_OK_STATUS).json(talkers);
     }
   } catch (error) {
-    res.status(500).json({ error: 'Palestrante não encontrado' });
+    res.status(500).json({ error: 'Erro ao ler arquivo' });
+  }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  try {
+    const data = await fs.readFile('src/talker.json', 'utf8');
+    const talkers = JSON.parse(data);
+
+    const foundTalker = talkers.find((talker) => talker.id === id);
+
+    if (foundTalker) {
+      res.status(HTTP_OK_STATUS).json(foundTalker);
+    } else {
+      res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao ler arquivo.' });
   }
 });
 
